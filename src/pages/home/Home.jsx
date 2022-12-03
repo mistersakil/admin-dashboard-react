b2a58fd8aeb040948f90c65442cc3acbe6b3e526
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-12-03 20:53:51 +0600

    homepage card, graph component design done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -17,16 +17,44 @@ const Home = () => {
   let cardsList = cards.map((cardSingle) => {
     return <Card {...cardSingle}></Card>;
   });
+
+  /**
+   * graphList
+   * generate graph
+   */
+  const graphs = [
+    {
+      title: "Last 6 months spending",
+      data: [
+        { month: "Jan", amount: "700" },
+        { month: "Feb", amount: "300" },
+        { month: "Mar", amount: "700" },
+        { month: "Apr", amount: "200" },
+        { month: "May", amount: "600" },
+        { month: "Jun", amount: "850" },
+      ],
+    },
+    {
+      title: "Last 6 months revenue",
+      data: [
+        { month: "Jan", amount: "300" },
+        { month: "Feb", amount: "700" },
+        { month: "Mar", amount: "200" },
+        { month: "Apr", amount: "950" },
+        { month: "May", amount: "350" },
+        { month: "Jun", amount: "550" },
+      ],
+    },
+  ];
+  let graphList = graphs.map((singleItem) => {
+    return <Graph {...singleItem}></Graph>;
+  });
   return (
     <Layout>
       <main className="home">
         <Widgets />
-
         <section className="cardRow">{cardsList}</section>
-        <section className="graphRow">
-          <Graph />
-          <Graph />
-        </section>
+        <section className="graphRow">{graphList}</section>
       </main>
     </Layout>
   );
